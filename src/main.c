491eb76c300c0e06aa92c0d81b6da9c40ec3d98b
Layton <caolei6767@google.com>
2023-07-17 23:21:59 +0800

    mock func

--- src/main.c
+++ src/main.c
@@ -3,76 +3,11 @@
 #include "led.h"
 
 
-void _close(void)
-{
-}
-void _lseek(void)
-{
-}
-void _read(void)
-{
-}
-void _write(void)
-{
-}
-void _kill(void)
-{
-}
-void _getpid(void)
-{
-}
-
-void setLedBlue(int state)
-{
-    if (!state)
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
-    else
-        HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_SET);
-}
-
-void setLedRed(int state)
-{
-    if (!state)
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);
-    else
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_SET);
-}
-
 void sleepSecond(int second)
 {
     HAL_Delay(second * 1000);
 }
 
-
-void GPIOC5_REDLED_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_5, GPIO_PIN_RESET);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_5;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-}
-
-void GPIOB2_BLUELED_Init(void)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2, GPIO_PIN_RESET);
-
-    GPIO_InitStruct.Pin = GPIO_PIN_2;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-}
 
 void SysTick_Handler(void)
 {
