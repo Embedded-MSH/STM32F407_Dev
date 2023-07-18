83d671bf66cb9e3338d396fe86750309dd148b86
Layton <caolei6767@google.com>
2023-07-18 22:16:16 +0800

    add key ctrl

--- Include/led.h
+++ Include/led.h
@@ -1,6 +1,15 @@
 #ifndef __LED__H__
 #define __LED__H__
 
+/***************************** Config *******************************/
+// LED_RED
+#define LED_RED_GPIO GPIOC
+#define LED_RED_PIN GPIO_PIN_5
+// LED_BLUE
+#define LED_BLUE_GPIO GPIOB
+#define LED_BLUE_PIN GPIO_PIN_2
+/***************************** Config End *******************************/
+
 enum led_id
 {
     LED_RED = 0,
--- src/led.c
+++ src/led.c
@@ -1,24 +1,19 @@
 #include "led.h"
 
 #include <stm32f4xx_hal.h>
-void led_init(enum led_id id)
-{
-    if (id >= LED_TOTAL)
-        return;
 
+
+static void led_init_one(enum led_id id)
+{
     GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-    GPIO_TypeDef* gpio_port = id == LED_BLUE ? GPIOB : GPIOC;
-    unsigned int gpio_pin = id == LED_BLUE ? GPIO_PIN_2 : GPIO_PIN_5;
+    GPIO_TypeDef* gpio_port = id == LED_BLUE ? LED_BLUE_GPIO : LED_RED_GPIO;
+    unsigned int gpio_pin = id == LED_BLUE ? LED_BLUE_PIN : LED_RED_PIN;
 
     if (gpio_port == GPIOB)
         __HAL_RCC_GPIOB_CLK_ENABLE();
     else if (gpio_port == GPIOC)
         __HAL_RCC_GPIOC_CLK_ENABLE();
-    else
-        while (1) {
-            __asm("nop");
-        }
 
     HAL_GPIO_WritePin(gpio_port, gpio_pin, GPIO_PIN_RESET);
 
@@ -29,23 +24,58 @@ void led_init(enum led_id id)
     HAL_GPIO_Init(gpio_port, &GPIO_InitStruct);
 }
 
-void led_set(enum led_id id, enum led_state state)
+
+static void led_set_one(enum led_id id, enum led_state state)
 {
-    if (id >= LED_TOTAL)
-        return;
-
-    GPIO_TypeDef* gpio_port = id == LED_BLUE ? GPIOB : GPIOC;
-    unsigned int gpio_pin = id == LED_BLUE ? GPIO_PIN_2 : GPIO_PIN_5;
-
+    GPIO_TypeDef* gpio_port = id == LED_BLUE ? LED_BLUE_GPIO : LED_RED_GPIO;
+    unsigned int gpio_pin = id == LED_BLUE ? LED_BLUE_PIN : LED_RED_PIN;
     GPIO_PinState pin_state = state == LED_ON ? GPIO_PIN_SET : GPIO_PIN_RESET;
     HAL_GPIO_WritePin(gpio_port, gpio_pin, pin_state);
 }
 
+
+static void led_toggle_one(enum led_id id)
+{
+    GPIO_TypeDef* gpio_port = id == LED_BLUE ? LED_BLUE_GPIO : LED_RED_GPIO;
+    unsigned int gpio_pin = id == LED_BLUE ? LED_BLUE_PIN : LED_RED_PIN;
+    HAL_GPIO_TogglePin(gpio_port, gpio_pin);
+}
+
+void led_init(enum led_id id)
+{
+    if (id > LED_TOTAL)
+        return;
+    if (id == LED_TOTAL) {
+        for (int i = 0; i < LED_TOTAL; i++)
+            led_init_one(i);
+    } else {
+        led_init_one(id);
+    }
+}
+
+
+
+void led_set(enum led_id id, enum led_state state)
+{
+    if (id > LED_TOTAL)
+        return;
+
+    if (id == LED_TOTAL) {
+        for (int i = 0; i < LED_TOTAL; i++)
+            led_set_one(i, state);
+    } else {
+        led_set_one(id, state);
+    }
+}
+
 void led_toggle(enum led_id id)
 {
-    if (id >= LED_TOTAL)
+    if (id > LED_TOTAL)
         return;
-    GPIO_TypeDef* gpio_port = id == LED_BLUE ? GPIOB : GPIOC;
-    unsigned int gpio_pin = id == LED_BLUE ? GPIO_PIN_2 : GPIO_PIN_5;
-    HAL_GPIO_TogglePin(gpio_port, gpio_pin);
+    if (id == LED_TOTAL) {
+        for (int i = 0; i < LED_TOTAL; i++)
+            led_toggle_one(i);
+    } else {
+        led_toggle_one(id);
+    }
 }
